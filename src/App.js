039686b6335e2e07cfd024e4479ea01cd8f59dd7
robbie <robbiekruszynski@gmail.com>
2020-02-11 13:41:48 -0700

    add form to use and check to see if the results are being logged

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { ReactTypeformEmbed } from "react-typeform-embed";
 
 export default class App extends React.Component {
   render() {
-    return <ReactTypeformEmbed url="https://demo.typeform.com/to/njdbt5" />;
+    return (
+      <ReactTypeformEmbed url="https://robbiekruszynski.typeform.com/to/NlEfNp" />
+    );
   }
 }
